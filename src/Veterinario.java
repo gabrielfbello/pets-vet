5ee7ebe93b6e5a296568a0f6497de2e843c5cba7
Gabriel F. Bello <gfbbello@outlook.com>
2022-09-15 20:06:30 -0300

    feat: Veterinario Class

---
+++ src/Veterinario.java
@@ -0,0 +1,65 @@
+public class Veterinario {
+    private String nome;
+    private String crmv;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private String dataCadastro;
+    private String dataUltimaConsulta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrmv() {
+        return crmv;
+    }
+
+    public void setCrmv(String crmv) {
+        this.crmv = crmv;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getDataUltimaConsulta() {
+        return dataUltimaConsulta;
+    }
+
+    public void setDataUltimaConsulta(String dataUltimaConsulta) {
+        this.dataUltimaConsulta = dataUltimaConsulta;
+    }
+}
