910d76d39ba6ce377f0ee949e1a7560f4e3103c3
Gabriel F. Bello <gfbbello@outlook.com>
2022-09-15 20:04:01 -0300

    feat: Proprietario Class

---
+++ src/Proprietario.java
@@ -0,0 +1,65 @@
+public class Proprietario {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private String dataCadastro;
+    private String dataUltimaConsulta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getDataUltimaConsulta() {
+        return dataUltimaConsulta;
+    }
+
+    public void setDataUltimaConsulta(String dataUltimaConsulta) {
+        this.dataUltimaConsulta = dataUltimaConsulta;
+    }
+}
